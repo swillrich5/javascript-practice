a4874501ca1e493b559da6ba2cff0d40d707ee8c
Scott Willrich <swillrich@gmail.com>
2022-12-16 16:06:45 -0600

    added lesson 49

---
+++ lesson49/sandbox.js
@@ -0,0 +1,12 @@
+const title = document.querySelector('h1');
+
+// title.setAttribute('style', 'margin: 50px');
+
+console.log(title.style);
+console.log(title.style.color);
+console.log(title.style.margin);
+
+title.style.margin = '50px';
+title.style.color = 'crimson';
+title.style.fontSize = '60px';
+title.style.fontFamily = 'Comic Sans MS';
